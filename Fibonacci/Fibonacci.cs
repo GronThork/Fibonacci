cb9e733ce45016f041f452678eef2b7fc78ff3f6
salvarez <salvarez@voxelgroup.net>
2021-11-17 11:00:37 +0100

    Created Fibonacci method

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci
+{
+    public static class Fibonacci
+    {
+        public static int CalculateFibonacci(int n)
+        {
+            return n switch
+            {
+                0 => 0,
+                1 => 1,
+                _ => n - 1 + n - 2,
+            };
+        }
+    }
+}
--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,22 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nElement;
+            string exit; 
+
+            do
+            {
+                Console.WriteLine("Write n element: ");
+                nElement = Convert.ToInt32(Console.ReadLine());
+
+                if (Fibonacci.CalculateFibonacci(nElement) != nElement)
+                    Console.WriteLine($"For element {nElement}, the return isnt {nElement}!");
+
+                Console.WriteLine("Do you want to exit (Y/N): ");
+                exit = Console.ReadLine();
+            } while (exit.Equals("N"));
+
+
         }
     }
 }
